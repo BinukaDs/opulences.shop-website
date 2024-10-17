51524ecb5b9aa686486a6d8f0737fb27624216a1
Binuka Dasunpriya <binukadasunpriya@gmail.com>
2024-10-17 13:25:02 +0530

    cors changes

--- client/src/components/sections/PricingSection.tsx
+++ client/src/components/sections/PricingSection.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { Card, CardContent, CardHeader, CardDescription, CardTitle, CardFooter } from "../ui/card"
+
 import { Button } from "../ui/button"
 import Image from "next/image"
 import TopicFade from "@/app/transitions/TopicFade"
@@ -17,22 +18,7 @@ const PricingSection = () => {
         button2: false,
     });
     const BASEURL = process.env.NEXT_PUBLIC_BASE_URL
-    const test = () => {
-        fetch(`${BASEURL}/`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-           
-        }).then(res => {
-            if (res.ok) return res.json()
-            return res.json().then(json => Promise.reject(json))
-        }).then(({ url }) => {
-            console.log(url)
-        }).catch(error => {
-            console.error(error)
-        })
-    }
+   
     const purchase = (e: any) => {
 
         let item: { id: number; price: number; name: string }[] = []
@@ -182,7 +168,7 @@ const PricingSection = () => {
                                         </div>
                                     </CardContent>
                                     <CardFooter className="w-full flex flex-col justify-between items-center">
-                                        <Button className="w-full bg-black hover:bg-black/80 text-white text-lg" onClick={test} id="button2" disabled={loadingStates.button2}>
+                                        <Button className="w-full bg-black hover:bg-black/80 text-white text-lg" onClick={purchase} id="button2" disabled={loadingStates.button2}>
                                             {loadingStates.button2 ?
                                                 <PuffLoader size={40} color="#FFF" /> : "Purchase"
                                             }</Button>
